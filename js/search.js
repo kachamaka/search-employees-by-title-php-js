59c0e629266c90f67695cea0589d4a5da39a546e
kachamaka <minecraftlion@abv.bg>
2018-06-10 12:44:54 +0300

    fix

--- js/search.js
+++ js/search.js
@@ -9,40 +9,45 @@ xhttp.open("GET", "../php/fetch_emp.php", true);
 xhttp.send();
 
 function loadEmps(employees){
-    for(var i = 0; i<employees.length; i++){
+    for(let i = 0; i<employees.length; i++){
         emps.push(employees[i]);
     }
 }
 
 function search(){
-    var tbody = document.getElementById("tbody");
+    let tbody = document.getElementById("tbody");
     while(tbody.firstChild) {
         tbody.removeChild(tbody.firstChild);
     }
 
-    var title = document.getElementById("title");
+    let title = document.getElementById("title");
     let selectedTitle = this.title.options[title.selectedIndex].text;
     
     for(let i = 0; i<emps.length; i++){
         if(emps[i].title==selectedTitle){
-        var tr = document.createElement("tr"); 
-        var first_name = document.createElement("td");
-        var last_name = document.createElement("td");
-        var birth_year = document.createElement("td");
-        var monthly_salary = document.createElement("td");   
-        var first_nameVal = document.createTextNode(emps[i].first_name);
-        var last_nameVal = document.createTextNode(emps[i].last_name);
-        var birth_yearVal = document.createTextNode(emps[i].year_of_birth);
-        var monthly_salaryVal = document.createTextNode("$" + emps[i].monthly_salary);        
-        first_name.appendChild(first_nameVal);
-        last_name.appendChild(last_nameVal);
-        birth_year.appendChild(birth_yearVal);
-        monthly_salary.appendChild(monthly_salaryVal);
-        tr.appendChild(first_name);
-        tr.appendChild(last_name);
-        tr.appendChild(birth_year);
-        tr.appendChild(monthly_salary);
-        tbody.appendChild(tr);	
+            addEmp(emps, i, tbody);
         }
     }
+}
+
+function addEmp(emps, i, tbody){
+    let tr = document.createElement("tr"); 
+    let first_name = document.createElement("td");
+    let last_name = document.createElement("td");
+    let birth_year = document.createElement("td");
+    let monthly_salary = document.createElement("td");   
+    let first_nameVal = document.createTextNode(emps[i].first_name);
+    let last_nameVal = document.createTextNode(emps[i].last_name);
+    let birth_yearVal = document.createTextNode(emps[i].year_of_birth);
+    let monthly_salaryVal = document.createTextNode("$" + emps[i].monthly_salary);       
+
+    first_name.appendChild(first_nameVal);
+    last_name.appendChild(last_nameVal);
+    birth_year.appendChild(birth_yearVal);
+    monthly_salary.appendChild(monthly_salaryVal);
+    tr.appendChild(first_name);
+    tr.appendChild(last_name);
+    tr.appendChild(birth_year);
+    tr.appendChild(monthly_salary);
+    tbody.appendChild(tr);	
 }
